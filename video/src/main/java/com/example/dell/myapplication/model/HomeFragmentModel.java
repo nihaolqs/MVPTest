2fc2e5aa9e9c6693abfd7cc11cb2dd82854c59c0
lin <nihaolqs@163.com>
2016-09-06 00:28:33 +0800

    Gson依赖

--- video/src/main/java/com/example/dell/myapplication/model/HomeFragmentModel.java
+++ video/src/main/java/com/example/dell/myapplication/model/HomeFragmentModel.java
@@ -1,7 +1,15 @@
 package com.example.dell.myapplication.model;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.VolleyError;
 import com.example.dell.myapplication.app.Constants;
 import com.example.dell.myapplication.bean.VideoBean;
+import com.example.dell.myapplication.utils.HttpResponseListener;
+import com.example.dell.myapplication.utils.HttpUtil;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -9,6 +17,13 @@ import java.util.List;
  * Created by dell on 2016/9/5.
  */
 public class HomeFragmentModel implements IBannerModel, IVideoListModel{
+
+    private Context mContext;
+
+    public HomeFragmentModel(Context context)
+    {
+        this.mContext = context;
+    }
     @Override
     public List<VideoBean> getBannerList() {
         return null;
@@ -22,5 +37,17 @@ public class HomeFragmentModel implements IBannerModel, IVideoListModel{
     public void replaceData()
     {
         String urlStr = Constants.ApiCline.VIDEO_HOME;
+        HttpResponseListener listener = new HttpResponseListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(mContext,volleyError.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Object o) {
+
+            }
+        };
+        HttpUtil.httpStringGetReques(mContext,urlStr,listener);
     }
 }
